dd1fdd845e210688bf89dccf55780b6bc3d298d2
Onkar Patel <onkarpatel10@gmail.com>
2025-07-11 15:23:53 +0530

    feat: add Jason character section with scroll reveal animation and content layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { ScrollTrigger } from 'gsap/all';
 import NavBar from './sections/NavBar.jsx';
 import Hero from './sections/Hero.jsx';
 import FirstVideo from './sections/FirstVideo.jsx';
+import Jason from './sections/Jason.jsx';
+import SecondVideo from './sections/SecondVideo.jsx';
 
 // Register it once globally
 // This is necessary to use ScrollTrigger in your project
@@ -14,6 +16,7 @@ const App = () => {
       <NavBar />
       <Hero />
       <FirstVideo />
+      <Jason />
     </main>
   );
 };
