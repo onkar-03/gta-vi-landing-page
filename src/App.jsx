60cdb2d163a9e5516cc1a8e23fbdf7d00b0d1157
Onkar Patel <onkarpatel10@gmail.com>
2025-07-11 16:37:48 +0530

    feat: add Lucia life section with scroll-triggered animations and responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './sections/Hero.jsx';
 import FirstVideo from './sections/FirstVideo.jsx';
 import Jason from './sections/Jason.jsx';
 import SecondVideo from './sections/SecondVideo.jsx';
+import Lucia from './sections/Lucia.jsx';
 
 // Register it once globally
 // This is necessary to use ScrollTrigger in your project
@@ -19,6 +20,7 @@ const App = () => {
       <Jason />
 
       <SecondVideo />
+      <Lucia />
     </main>
   );
 };
